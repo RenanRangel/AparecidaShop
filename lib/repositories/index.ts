afc3f31675f9a16756cf7e25454be606729baec8
Renan Rangel <renanrangel.augusto10@gmail.com>
2026-07-28 20:25:52 -0300

    fix: Mudanças na home/lojas

--- lib/repositories/index.ts
+++ lib/repositories/index.ts
@@ -1,6 +1,5 @@
 import { PrismaStoreRepository } from './prisma/store.repository'; // ou o caminho que você usou
 import { PrismaProductRepository } from './prisma/product.repository';
-import type { StoreRepository, ProductRepository } from './types';
 
 export const storeRepository = new PrismaStoreRepository();
 export const productRepository = new PrismaProductRepository();

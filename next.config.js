463060e1bfeb897184be72fa2235d90d1d0c5516
RenanRangel <renanrangel.augusto10@gmail.com>
2026-08-17 20:38:13 -0300

    feat: Arrumando imagens

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
-
-module.exports = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '*.public.blob.vercel-storage.com' },
